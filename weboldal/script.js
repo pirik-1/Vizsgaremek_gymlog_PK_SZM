d5bd7f2a76d779a95c7ecbf9ecf3169ca9963b80
pirik-1 <kingsroas1@gmail.com>
2025-11-15 18:06:18 +0100

    mutasd a jelszót működik

---
+++ weboldal/script.js
@@ -0,0 +1,13 @@
+let jelszo = document.getElementById("jelszo");
+let mutasd = document.getElementById("mutasd");
+
+mutasd.addEventListener("mouseover", function(){
+    jelszo.type = "text";
+    mutasd.style.color = "lightblue";
+})
+mutasd.addEventListener("mouseout", function(){
+    jelszo.type = "password";
+    mutasd.style.color = "white"
+})
+
+
